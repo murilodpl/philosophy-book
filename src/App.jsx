3fbee46ccfd1612ddd2b49843da2dab8c6c7b965
Murilo <inayievmpl@gmail.com>
2022-02-15 17:55:24 -0300

    Update App.jsx - Import components - Do API request, save and show

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+
+import Navbar from "./components/Navbar"
+import Philosopher from "./components/Philosopher"
+
 export default function App() {
+  // Variables
+  const [philosophers, setPhilosophers] = useState({})
+
+  // API Resquests
+  useEffect(() => {
+    const philosopherId = Math.ceil(Math.random() * 12) // The api has 12 philosophers
+    fetch(`https://philosophyapi.herokuapp.com/api/philosophers/${philosopherId}`)
+      .then(res => res.json())
+      .then(data => {
+        setPhilosophers(data)
+      })
+  }, [])
+
+  // Console logs
+  console.log(philosophers)
+
+  // Functions
+
+
+  // Components population
+
+
   return (
     <div className="App">
-      <h1 className="text-xl font-bold mt-4">Philosophy Book</h1>
+      <Navbar />
+
+      <section>
+        <Philosopher data={philosophers} />
+      </section>{/* Search Section */}
     </div>
   )
 } 
