0946afa404f8dd1354db2bef53d8b4ac85d9e4af
Murilo <inayievmpl@gmail.com>
2022-02-15 15:40:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { useState } from 'react'
-
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-      </header>
+      <h1 className="text-xl font-bold mt-4">Philosophy Book</h1>
     </div>
   )
-}
+} 
