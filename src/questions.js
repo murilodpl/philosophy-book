4db6e6b72e7da228d9732aa80ae966d7697c167a
Murilo <mdpleopoldino@gmail.com>
2022-02-21 13:57:51 -0300

    Add questions database

---
+++ src/questions.js
@@ -0,0 +1,446 @@
+[
+    { site: "https://ismailyusufcollege.com/wp-content/uploads/2020/10/Western-Philosophy-MCQS.pdf" },
+    {
+        id: 1,
+        question: "The four main divisions of philosophy are metaphysics, epistemology, axiology, and _____.",
+        answers: [
+            "bioethics",
+            "logic",
+            "aesthetics",
+            "categorical logic",
+        ],
+        correct_answer: "logic"
+    },
+    {
+        id: 2,
+        question: "The study of reality in the broadest sense, an inquiry into the elemental nature of the universe and the things in it, is known as _____.",
+        answers: [
+            "metaphysics",
+            "epistemology",
+            "quantum physics",
+            "axiology",
+        ],
+        correct_answer: "metaphysics"
+    },
+    {
+        id: 3,
+        question: "Questions like “What is knowledge?” and “What is truth?” are mainstays in the branch of philosophy known as _____.",
+        answers: [
+            "logic",
+            "metaphysics",
+            "epistemology",
+            "aesthetics",
+        ],
+        correct_answer: "epistemology"
+    },
+    {
+        id: 4,
+        question: "For Socrates, an unexamined life is a tragedy because it results in grievous harm to _____.",
+        answers: [
+            "the state",
+            "the justice system",
+            "the body",
+            "the soul",
+        ],
+        correct_answer: "the soul"
+    },
+    {
+        id: 5,
+        question: "For Socrates, the soul is harmed by lack of _____.",
+        answers: [
+            "knowledge",
+            "wealth",
+            "community",
+            "courage",
+        ],
+        correct_answer: "knowledge"
+    },
+    {
+        id: 6,
+        question: "A question-and-answer dialogue in which propositions are methodically scrutinized to uncover the truth is known as _____.",
+        answers: [
+            "an argument",
+            "the Socratic method",
+            "the Socratic jest",
+            "a debate",
+        ],
+        correct_answer: "the Socratic method"
+    },
+    {
+        id: 7,
+        question: "According to Socrates, a clear sign that a person has _____ is her exclusive pursuit of social status, wealth, power, and pleasure.",
+        answers: [
+            "philosophical ambition",
+            "worldly wisdom",
+            "exceptional desires",
+            "an unhealthy soul",
+        ],
+        correct_answer: "an unhealthy soul"
+    },
+    {
+        id: 8,
+        question: "The famous statement “An unexamined life is not worth living” is attributed to _____.",
+        answers: [
+            "Aristotle",
+            "John Locke",
+            "Socrates",
+            "Plato",
+        ],
+        correct_answer: "Socrates"
+    },
+    {
+        id: 10,
+        question: "——— is a Skeptic.",
+        answers: [
+            "David Hume",
+            "Emmanuel Kant",
+            "Hegel",
+            "None of these",
+        ],
+        correct_answer: "David Hume"
+    },
+    {
+        id: 11,
+        question: "Esseest Percipi is the concept of:",
+        answers: [
+            "Rene Descartes",
+            "Benedict Spinoza",
+            "George Berkeley",
+            "Francis Bacon",
+        ],
+        correct_answer: "George Berkeley"
+    },
+    {
+        id: 12,
+        question: "The metaphysical view that there are many realities is known as",
+        answers: [
+            "Dualism",
+            "Pluralism",
+            "Monism",
+            "Non-dualism",
+        ],
+        correct_answer: "Pluralism"
+    },
+    {
+        id: 13,
+        question: "The theory that holds reason as the source of knowledge is",
+        answers: [
+            "Idealism",
+            "Rationalism",
+            "Empiricism",
+            "None of the above",
+        ],
+        correct_answer: "Rationalism"
+    },
+    {
+        id: 14,
+        question: "Inborn ideas are known as:",
+        answers: [
+            "Adventitious ideas",
+            "Factitious ideas",
+            "Innate ideas",
+            "a, b & c",
+
+        ],
+        correct_answer: "Innate ideas"
+    },
+    {
+        id: 15,
+        question: "The Philosophy of Hegel is known as",
+        answers: [
+            "Phenomenal idealism",
+            "Personal idealism",
+            "Objective idealism",
+            "Absolute idealism",
+        ],
+        correct_answer: "Absolute idealism"
+    },
+    {
+        id: 16,
+        question: "The founder of Positivism is",
+        answers: [
+            "Saint Simon",
+            "C. S Pierce",
+            "August Comte",
+            "Francis Bacon",
+        ],
+        correct_answer: "August Comte"
+    },
+    {
+        id: 17,
+        question: "The author of Novum Organum is",
+        answers: [
+            "Rene Descartes",
+            "Lemnitzer",
+            "John Dewey",
+            "Francis Bacon",
+        ],
+        correct_answer: "John Dewey"
+    },
+    {
+        id: 18,
+        question: "The science of values is known as",
+        answers: [
+            "Axiology",
+            "Sociology",
+            "Ontology",
+            "Cosmology",
+        ],
+        correct_answer: "Axiology"
+    },
+    {
+        id: 19,
+        question: "Metaphysics is the work of",
+        answers: [
+            "Socrates",
+            "Plato",
+            "Aristotle",
+            "Thales",
+        ],
+        correct_answer: "Aristotle"
+    },
+    {
+        id: 21,
+        question: "Early Skepticism is also known as",
+        answers: [
+            "Pyrrhonism",
+            "Positivism",
+            "Materialism",
+            "Idealism",
+        ],
+        correct_answer: "Pyrrhonism"
+    },
+    {
+        id: 20,
+        question: "The Greek word Pragma means",
+        answers: [
+            "Truth",
+            "Act or deed",
+            "Utility",
+            "Beauty",
+        ],
+        correct_answer: "Act or deed"
+    },
+    {
+        id: 22,
+        question: "The author of Republic",
+        answers: [
+            "Socrates",
+            "Descartes",
+            "Plato",
+            "Hume",
+        ],
+        correct_answer: "Plato"
+    },
+    {
+        id: 23,
+        question: "The metaphysical view that there is one ultimate reality is called",
+        answers: [
+            "Dualism",
+            "Pluralism",
+            "Monism",
+            "None of these",
+        ],
+        correct_answer: "Monism"
+    },
+    {
+        id: 24,
+        question: "According to the modern scientists matter is",
+        answers: [
+            "Light",
+            "Energy",
+            "Illusion",
+            "All of these",
+        ],
+        correct_answer: "Energy"
+    },
+    {
+        id: 25,
+        question: "——— is a Greek atomist",
+        answers: [
+            "Democritus",
+            "Xenophanes",
+            "Xenophon",
+            "Zeno",
+
+        ],
+        correct_answer: "Democritus"
+    },
+    {
+        id: 26,
+        question: "epistemology studies",
+        answers: [
+            "Origin and Nature of knowledge",
+            "Validity of knowledge",
+            "Extendofknowledge",
+            "a, b & c",
+        ],
+        correct_answer: "a, b & c"
+    },
+    {
+        id: 27,
+        question: "The two branches of Metaphysics are",
+        answers: [
+            "Religion and Theology",
+            "Rationalism and Empiricism",
+            "Ontology and Cosmology",
+            "Materialism and Spiritualism",
+        ],
+        correct_answer: "Ontology and Cosmology"
+    },
+    {
+        id: 28,
+        question: "The three distempers of learning according to. Francis Bacon are:",
+        answers: [
+            "Mind, body and God",
+            "Ideas, experience and thought.",
+            "Fantastical, contentious and delicate",
+            "Tribe, cave and theatre",
+        ],
+        correct_answer: "Fantastical, contentious and delicate"
+    },
+    {
+        id: 29,
+        question: "Philosophy is concerned with",
+        answers: [
+            "The irrational",
+            "Beauty",
+            "The ideal",
+            "Experimentation",
+        ],
+        correct_answer: "The ideal"
+    },
+    {
+        id: 30,
+        question: "Ethics is a ———",
+        answers: [
+            "Positive science",
+            "Normative science",
+            "Descriptive science",
+            "None of these",
+        ],
+        correct_answer: "Normative science"
+    },
+    {
+        id: 31,
+        question: "——— seeks to clarify and refine the process of knowing",
+        answers: [
+            "Epistemology",
+            "Axiology",
+            "Metaphysics",
+            "Ethics",
+        ],
+        correct_answer: "Epistemology"
+    },
+    {
+        id: 32,
+        question: "The Latin word ——— is the root of rationalism",
+        answers: [
+            "Sophia",
+            "Reason",
+            "Ratio",
+            "Experiential",
+        ],
+        correct_answer: "Ratio"
+    },
+    {
+        id: 33,
+        question: "———— insists on a a priori knowledge.",
+        answers: [
+            "Positivism",
+            "Empiricism",
+            "Rationalism",
+            "None of these",
+        ],
+        correct_answer: "Rationalism"
+    },
+    {
+        id: 38,
+        question: "Berkeley argues that all knowledge is derived from",
+        answers: [
+            "Impression",
+            "Ideas",
+            "Axioms",
+            "Postulates",
+        ],
+        correct_answer: "Ideas"
+    },
+    {
+        id: 39,
+        question: "———— helps in getting correct knowledge through anumana",
+        answers: [
+            "Vyapti",
+            "Paksha",
+            "Sadhya",
+            "Badha",
+        ],
+        correct_answer: "Vyapti"
+    },
+    {
+        id: 40,
+        question: "The words of a trustworthy person is known as",
+        answers: [
+            "Satya",
+            "Dharma",
+            "Aptavakya",
+            "Linga",
+        ],
+        correct_answer: "Aptavakya"
+    },
+    {
+        id: 43,
+        question: "A theory is a conclusion, where as a method is a",
+        answers: [
+            "Style",
+            "Manner",
+            "Inference",
+            "Procedure",
+        ],
+        correct_answer: "Procedure"
+    },
+    {
+        id: 44,
+        question: "Philosophy is the root and science is the",
+        answers: [
+            "Fruit",
+            "Branch",
+            "Nourishment",
+            "Stem",
+        ],
+        correct_answer: "Fruit"
+    },
+
+    {
+        id: 45,
+        question: "Who said ? Philosophy is the Science of sciences",
+        answers: [
+            "Plato",
+            "Voltaire",
+            "August's Comte",
+            "Russell",
+        ],
+        correct_answer: "August's Comte"
+    },
+    {
+        id: 46,
+        question: "———— said “whatever is. Clearly and distinctly perceived is true”",
+        answers: [
+            "Locke",
+            "Berkeley",
+            "Descartes",
+            "None of these",
+        ],
+        correct_answer: "Descartes"
+    },
+    {
+        id: 47,
+        question: "According to Skepticism knowledge is ———",
+        answers: [
+            "Certain",
+            "Possible",
+            "Uncertain",
+            "None of these",
+        ],
+        correct_answer: "Uncertain"
+    }
+]
