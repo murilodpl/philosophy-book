861f1e4ac88578ba4b66cf6dc7cb4ce1a83f5955
Murilo <inayievmpl@gmail.com>
2022-02-22 16:10:28 -0300

    Update Philosopher.jsx - Add animation when changing philosopher

--- src/components/AnimatedPage.jsx
+++ src/components/AnimatedPage.jsx
@@ -13,7 +13,7 @@ const AnimatedPage = ({ children }) => {
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{ duration: 0.25 }}
+      transition={{ duration: 0.5 }}
     >
       {children}
     </motion.div>
