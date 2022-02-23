8dd64c975bcb21ede29b3d981c499af3eff95635
Murilo <inayievmpl@gmail.com>
2022-02-23 17:50:10 -0300

    Resolve CORS error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  // Set fetch '/api' to the philosophyapi (resolve CORS error)
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://philosophyapi.herokuapp.com/api",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
